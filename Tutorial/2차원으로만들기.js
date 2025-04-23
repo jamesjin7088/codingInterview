6245ac5e0b2f3dd36fcce391c3e53c2a99d63662
jamesjin7088 <wlstkdwns7088@gmail.com>
2025-04-23 12:04:08 +0900

    commit

--- Tutorial/2차원으로만들기.js
+++
@@ -1,9 +0,0 @@
-const solution = (num_list, n) => {
-  var answer = [];
-  for (var i = 0; i < num_list.length / n; i++) {}
-
-  console.log(answer);
-};
-
-solution([1, 2, 3, 4, 5, 6, 7, 8], 2);
-solution([100, 95, 2, 4, 5, 6, 18, 33, 948], 3);
